8db5d6d90f0c5e8709e1d3eccb9b9fb7358dc8f8
doncodie <don.codie@gmail.com>
2019-11-20 23:53:47 +0100

    Create front :    - boucle ngFor    - condition classes ngClass    - create type Post + constructor    - create components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   // Title
   title = 'posts';
-
   // Array posts
   posts = [
-    {
-      title: 'Mon premier post',
-      content: 'Thalassius vero ea tempestate praefectus praetorio praesens ipse quoque adrogantis ingenii,' +
-        ' considerans incitationem eius ad multorum augeri discrimina, non maturitate vel consiliis mitigabat,' +
-        ' ut aliquotiens celsae potestates iras principum molliverunt.',
-      loveIts: 1,
-      created_at: new Date()
-    },
-    {
-      title: 'Mon deuxième post',
-      content: 'Caelium in illo aetatis flore vidit nisi aut cum patre aut mecum aut in M. Crassi castissima domo, ' +
-        'cum artibus honestissimis erudiretur.',
-      loveIts: -1,
-      created_at: new Date()
-    },
-    {
-      title: 'Encore un post',
-      content: 'Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni' +
-        ' locum, fuit primum ipsius pudore, deinde etiam patris diligentia disciplinaque munita.',
-      loveIts: 0,
-      created_at: new Date()
-    },
-
+    new Post('Mon premier post', 'Thalassius vero ea tempestate praefectus praetorio praesens ipse quoque adrogantis ingenii,' +
+      ' considerans incitationem eius ad multorum augeri discrimina, non maturitate vel consiliis mitigabat,' +
+      ' ut aliquotiens celsae potestates iras principum molliverunt.', 1),
+    new Post('Mon deuxième post', 'Caelium in illo aetatis flore vidit nisi aut cum patre aut mecum aut in M. Crassi castissima domo', -1),
+    new Post('Encore un post', 'Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni' +
+      ' locum, fuit primum ipsius pudore, deinde etiam patris diligentia disciplinaque munita.', 0)
   ];
+}
 
-  // Function for counting opinions
-  onOpinionLove() {
-    this.loveIts + 1;
-  }
-  onOpinionNotlove() {
-    console.log('test');
+class Post {
+  title: string;
+  content: string;
+  loveIts: number;
+  createdAt: Date;
+
+  constructor(title, content, loveIts) {
+    this.title = title;
+    this.content = content;
+    this.loveIts = loveIts;
+    this.createdAt = new Date();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostListItemComponent } from './post-list-item/post-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostListItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'fr-CA' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
